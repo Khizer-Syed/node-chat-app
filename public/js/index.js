e22084c5da89a9dc16d76d9fa16b31222e6d2be1
Khizer Syed <khizer44@gmail.com>
2018-03-27 11:13:55 +0530

    Adding event listeners and emitters

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on('connect', function() {
+ console.log('connected to server');
+ // socket.emit('createEmail',{
+ //   to : 'khizer@gmail.com',
+ //   text : 'How are you'
+ // });
+
+ socket.emit('createMessage', {
+   from : 'depain',
+   text : 'Fine'
+ });
+});
+
+socket.on('disconnect', function() {
+console.log('Disconnected from Server');
+});
+
+// socket.on('newEmail', function(email) {
+//   console.log('New Email', email);
+// });
+
+socket.on('newMessage', function(message) {
+  console.log(message);
+});
--- server/server.js
+++ server/server.js
@@ -17,9 +17,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from : 'mike@example.com',
+  //   text : 'whats up',
+  //   createdAt : 1335522
+  // });
+  socket.emit('newMessage', {
+    from: 'khizer44',
+    text : 'wassup',
+    createdAt : new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log(message);
+  })
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
   socket.on('disconnect', () => {
     console.log('Disconnected from the client');
-  })
+  });
+
 });
 server.listen(port, () => {
   console.log(`Started on port ${port}`);
