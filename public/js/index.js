7782e9b26a40bcb9d6f04f62aa31d2d65f2456b4
Khizer Syed <khizer44@gmail.com>
2018-03-28 17:37:02 +0530

    Adding format timestamps using moment

--- public/js/index.js
+++ public/js/index.js
@@ -21,9 +21,9 @@ console.log('Disconnected from Server');
 // });
 
 socket.on('newMessage', function(message) {
-  console.log(message);
+  var formttedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from} : ${message.text}`);
+  li.text(`${message.from} ${formttedTime}: ${message.text}`);
   jQuery('#messages').append(li);
 });
 
@@ -63,9 +63,10 @@ locationButton.on('click', function() {
 });
 
 socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My Current Location</a>');
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
