51d65a2aafa03c02a8ccaed1c07764210bc2c005
Khizer Syed <khizer44@gmail.com>
2018-03-28 20:07:19 +0530

    If user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,17 @@
 var socket = io();
+
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(scrollTop + clientHeight + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
 socket.on('connect', function() {
  console.log('connected to server');
  // socket.emit('createEmail',{
@@ -29,6 +42,7 @@ socket.on('newMessage', function(message) {
     createdAt : formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formttedTime}: ${message.text}`);
   // jQuery('#messages').append(li);
@@ -79,6 +93,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My Current Location</a>');
   // li.text(`${message.from} ${formattedTime}: `);
